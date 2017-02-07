23a469ce8294aa69a2d828e68721716a0159a806
EPHYRA\Tian Song <e0003295@u.nus.edu>
2017-02-08 05:12:04 +0800

    Extract Contact class from Address,Email and Phone class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+
+    public String value;
+    protected boolean isPrivate;
+
+    public Contact() {
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+}
